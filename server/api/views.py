aa54a25c78ed54048b5e4e688d819c366c9f3ba2
An-try <andrey.anderey@gmail.com>
2020-12-25 01:03:19 +0200

    Implement the basic token auth end-point

--- server/api/views.py
+++ server/api/views.py
@@ -1,3 +1,34 @@
+from datetime import datetime
+
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import status
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
+from server.api.serializers import UserSerializer
+
+
+class AuthTokenLogin(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(
+            data=request.data,
+            context={'request': request}
+        )
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.validated_data['user']
+        user.last_login = datetime.now()
+        user.save()
+
+        token, created = Token.objects.get_or_create(user=user)
+
+        return Response(
+            {
+                'token': token.key,
+                'user': UserSerializer(user).data,
+            },
+            status=status.HTTP_200_OK
+        )
