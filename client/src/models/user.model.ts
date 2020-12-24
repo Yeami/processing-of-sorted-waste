37828ab26c2d612e3a948c9d7178031fab042d41
An-try <andrey.anderey@gmail.com>
2020-12-25 01:56:49 +0200

    Implement the user model on the front-end side

---
+++ client/src/models/user.model.ts
@@ -0,0 +1,11 @@
+export default interface UserModel {
+  id: number;
+  username: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  dateJoined: Date;
+  address: string;
+  role: string;
+}
