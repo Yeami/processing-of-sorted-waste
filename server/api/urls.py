3a714e5d9f50bfc3041a902e18ca1c5294bdbdf2
Yeami <ivanov.nikita@yeami.group>
2020-12-25 03:42:45 +0200

    Config the new routes to the three new end-points

--- server/api/urls.py
+++ server/api/urls.py
@@ -5,4 +5,7 @@ from server.api import views
 urlpatterns = [
     path('login', views.AuthTokenLogin.as_view()),
     path('me', views.Me.as_view()),
+    path('category', views.ListCategory.as_view()),
+    path('product', views.ListProduct.as_view()),
+    path('danger-level', views.ListDangerLevels.as_view()),
 ]
