19753b89abe157b37293ec7ae3209753389fb8c9
Yeami <ivanov.nikita@yeami.group>
2020-12-25 05:12:00 +0200

    Implement the order customer info component

---
+++ client/src/components/TheOrderCustomerInfo.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="customer-wrapper">
+    <span>Полное имя: {{customer.firstName}} {{customer.lastName}}</span>
+    <span>Имя пользователя: {{customer.username}}</span>
+    <span>Номер телефона: {{customer.phone}}</span>
+    <span>Почта: {{customer.email}}</span>
+    <span>Адрес проживания: {{customer.address}}</span>
+    <span>Дата регистрации: {{getDateJoined}}</span>
+    <br>
+    <span>Примечание к заказу: {{note}}</span>
+    <span>Дата создания заказа: {{getCreatedDate}}</span>
+  </div>
+</template>
+
+<script>
+import moment from 'moment';
+
+export default {
+  name: 'TheOrderCustomerInfo',
+  props: {
+    customer: Object,
+    note: String,
+    createdAt: String,
+  },
+  computed: {
+    getDateJoined() {
+      return moment(this.customer.dateJoined).format('DD.MM.YYYY');
+    },
+    getCreatedDate() {
+      return moment(this.createdAt).format('dddd, MMMM Do YYYY, h:mm:ss a');
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+
+</style>
