bebaa525309e75dcc299a10d0d958669842e916e
BoberMod <bobermod@yandex.ru>
2020-12-25 02:10:12 +0200

    Create the basic list of the store mutations

---
+++ client/src/store/mutations.type.ts
@@ -0,0 +1,8 @@
+export const SET_USER = 'SET_USER';
+export const SET_ERROR = 'SET_ERROR';
+export const SET_TOKEN = 'SET_TOKEN';
+export const SET_CATEGORIES = 'SET_CATEGORIES';
+export const SET_PRODUCTS = 'SET_PRODUCTS';
+export const UPDATE_BASKET = 'UPDATE_BASKET';
+export const CLEAR_BASKET = 'CLEAR_BASKET';
+export const SET_ORDERS = 'SET_ORDERS';
