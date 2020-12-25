1f91a18fc8773a603f157b86855a6efa28dee217
An-try <andrey.anderey@gmail.com>
2020-12-25 03:44:45 +0200

    Create new mutation to set the all danger levels

--- client/src/store/mutations.type.ts
+++ client/src/store/mutations.type.ts
@@ -2,6 +2,7 @@ export const SET_USER = 'SET_USER';
 export const SET_ERROR = 'SET_ERROR';
 export const SET_TOKEN = 'SET_TOKEN';
 export const SET_CATEGORIES = 'SET_CATEGORIES';
+export const SET_DANGER_LEVELS = 'SET_DANGER_LEVELS';
 export const SET_PRODUCTS = 'SET_PRODUCTS';
 export const UPDATE_BASKET = 'UPDATE_BASKET';
 export const CLEAR_BASKET = 'CLEAR_BASKET';
