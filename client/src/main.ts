046bd9b1d6d805ac7d905accfa10bf6bc97183f9
An-try <andrey.anderey@gmail.com>
2020-12-25 02:15:09 +0200

    Update the main.ts file

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,9 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import 'ant-design-vue/dist/antd.css';
 
-Vue.config.productionTip = false
+import Antd from 'ant-design-vue';
+import axios from 'axios';
+import Vue from 'vue';
+import VuexAxios from 'vue-axios';
+
+import { setAuthInterceptor, setAuthToken } from '@/services/auth.service';
+
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+Vue.config.productionTip = false;
+
+Vue.use(Antd);
+Vue.use(VuexAxios, axios);
+
+setAuthInterceptor();
+Vue.axios.defaults.baseURL = 'http://localhost:8000/api';
+const token = localStorage.getItem('auth-token');
+if (token) {
+  setAuthToken(token);
+}
 
 new Vue({
   router,
